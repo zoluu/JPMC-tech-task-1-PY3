86e228e6a4999f4edd763ae1443a913e5c69870c
zoluu <53238391+zoluu@users.noreply.github.com>
2019-10-27 21:43:34 +0000

    Task-001
    - Fixed functions in client3.py
    - Added unit tests in client_test.py

--- client_test.py
+++ client_test.py
@@ -1,5 +1,5 @@
 import unittest
-from client import getDataPoint
+from client3 import getDataPoint, getRatio
 
 class ClientTest(unittest.TestCase):
   def test_getDataPoint_calculatePrice(self):
@@ -7,19 +7,33 @@ class ClientTest(unittest.TestCase):
       {'top_ask': {'price': 121.2, 'size': 36}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 120.48, 'size': 109}, 'id': '0.109974697771', 'stock': 'ABC'},
       {'top_ask': {'price': 121.68, 'size': 4}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 117.87, 'size': 81}, 'id': '0.109974697771', 'stock': 'DEF'}
     ]
-    """ ------------ Add the assertion below ------------ """
+    for quote in quotes:
+      expected = (quote['stock'], quote['top_bid']['price'], quote['top_ask']['price'], (quote['top_bid']['price'] + quote['top_ask']['price'])/2)
+      self.assertEquals(getDataPoint(quote), expected)
 
   def test_getDataPoint_calculatePriceBidGreaterThanAsk(self):
     quotes = [
       {'top_ask': {'price': 119.2, 'size': 36}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 120.48, 'size': 109}, 'id': '0.109974697771', 'stock': 'ABC'},
       {'top_ask': {'price': 121.68, 'size': 4}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 117.87, 'size': 81}, 'id': '0.109974697771', 'stock': 'DEF'}
     ]
-    """ ------------ Add the assertion below ------------ """
+    for quote in quotes:
+      expected = (quote['stock'], quote['top_bid']['price'], quote['top_ask']['price'], (quote['top_bid']['price'] + quote['top_ask']['price'])/2)
+      self.assertEquals(getDataPoint(quote), expected)
+  
+  def test_getRatio_calculateRatio(self):
+    price_a = 2
+    price_b = 4
+    self.assertEquals(getRatio(price_a, price_b), 0.5)
 
+  def test_getRatio_calculateRatio2(self):
+    price_a = 3.5
+    price_b = 2
+    self.assertEquals(getRatio(price_a, price_b), 3.5/2)
 
-  """ ------------ Add more unit tests ------------ """
-
-
+  def test_getRatio_priceBIsZero(self):
+    price_a = 1
+    price_b = 0
+    self.assertEquals(getRatio(price_a, price_b), None)
 
 if __name__ == '__main__':
     unittest.main()
